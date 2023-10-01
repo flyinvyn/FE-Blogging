98bb8c43de1741c155dafbb07db04d4a29add3d8
Vynn <alfinyahyaalyashi13@gmail.com>
2023-10-01 22:35:00 +0700

    edit require auth

--- src/Auth/RequireAuth.jsx
+++ src/Auth/RequireAuth.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Navigate } from "react-router-dom";
 
 const RequireAuth = ({ children }) => {
-  const isAuth = localStorage.getItem("token_user");
+  const isAuth = localStorage.getItem("token");
   if (!isAuth) {
     return <Navigate to="/" replace="true" />;
   }
